535b1e3e56c58d2553fab055302276f384847f2a
matias-gonz <maigonzalez@fi.uba.ar>
2024-04-05 14:14:09 +0200

    fiubyte/.github#1 Add RegisterPage.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import HomePage from "./pages/HomePage/HomePage";
 import {ThemeProvider} from "@mui/material";
 import mainTheme from "./styles/themes";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
 
 function App() {
   return (
     <ThemeProvider theme={mainTheme}>
       <BrowserRouter>
         <Routes>
-          <Route path={'/'} Component={HomePage}/>
+          <Route path={'/'} Component={RegisterPage}/>
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
---
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -0,0 +1,11 @@
+import {Box, Typography} from "@mui/material";
+
+const RegisterPage = () => {
+  return (
+    <Box>
+      <Typography variant={"h3"}>¡Unite a BAFix!</Typography>
+    </Box>
+  );
+}
+
+export default RegisterPage;
