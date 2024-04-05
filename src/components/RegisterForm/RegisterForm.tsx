24a4e45d284e7bd04d70f4ab8cf30f44dc609a6d
matias-gonz <maigonzalez@fi.uba.ar>
2024-04-05 14:24:21 +0200

    fiubyte/.github#1 Add RegisterForm labels

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -19,49 +19,56 @@ const RegisterForm = () => {
 
   return (
     <form onSubmit={formik.handleSubmit} className={"RegisterForm"}>
+      <label htmlFor="name">Nombre</label>
       <input
         type="text"
         name="name"
         value={formik.values.name}
         onChange={formik.handleChange}
       />
+      <label htmlFor="surname">Apellido</label>
       <input
         type="text"
         name="surname"
         value={formik.values.surname}
         onChange={formik.handleChange}
       />
+      <label htmlFor="document_number">Número de documento</label>
       <input
         type="text"
         name="document_number"
         value={formik.values.document_number}
         onChange={formik.handleChange}
       />
+      <label htmlFor="address">Dirección</label>
       <input
         type="text"
         name="address"
         value={formik.values.address}
         onChange={formik.handleChange}
       />
+      <label htmlFor="phone_number">Número de teléfono</label>
       <input
         type="text"
         name="phone_number"
         value={formik.values.phone_number}
         onChange={formik.handleChange}
       />
+      <label htmlFor="max_radius">Radio máximo</label>
       <input
         type="number"
         name="max_radius"
         value={formik.values.max_radius}
         onChange={formik.handleChange}
       />
+      <label htmlFor="profile_photo_url">URL de la foto de perfil</label>
       <input
         type="text"
         name="profile_photo_url"
         value={formik.values.profile_photo_url}
         onChange={formik.handleChange}
       />
-      <button type="submit">Submit</button>
+      <button type="submit">Registrarse</button>
     </form>
   );
 }
