d1b511b69cce2b4dd6dd55ca7406a87e6a9f0d07
matias-gonz <maigonzalez@fi.uba.ar>
2024-04-13 20:24:39 +0200

    fiubyte/.github#5 Add admin login page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import SignInOrRegisterPage from "./pages/SignInOrRegisterPage/SignInOrRegisterPage";
 import MyServicesPage from "./pages/MyServicesPage/MyServicesPage";
 import AddServicePage from "./pages/AddServicePage/AddServicePage";
+import AdminLoginPage from "./pages/AdminLoginPage/AdminLoginPage";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
             <Route path={'/registro'} Component={RegisterPage}/>
             <Route path={'/mis-servicios'} Component={MyServicesPage}/>
             <Route path={'/agregar-servicio'} Component={AddServicePage}/>
+            <Route path={'/admin'} Component={AdminLoginPage}/>
           </Routes>
         </BrowserRouter>
       </StyledEngineProvider>
---
+++ src/pages/AdminLoginPage/AdminLoginPage.tsx
@@ -0,0 +1,11 @@
+import {Box} from "@mui/material";
+import "./AdminLoginPage.css"
+
+const AdminLoginPage = () => {
+  return (
+    <Box className={"AdminLoginPage"}>
+    </Box>
+  )
+}
+
+export default AdminLoginPage;
