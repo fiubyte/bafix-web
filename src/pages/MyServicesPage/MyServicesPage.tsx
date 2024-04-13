dccfb4d194916b298c46a6dd77c0d910850b78d0
matias-gonz <maigonzalez@fi.uba.ar>
2024-04-13 16:56:23 +0200

    fiubyte/.github#3 Add MyServicesPage banner

--- src/pages/MyServicesPage/MyServicesPage.tsx
+++ src/pages/MyServicesPage/MyServicesPage.tsx
@@ -1,10 +1,15 @@
-import {Box} from "@mui/material";
+import {Box, Typography} from "@mui/material";
 import Navbar from "../../components/Navbar/Navbar";
+import React from "react";
+import './MyServicesPage.css';
 
 const MyServicesPage = () => {
   return (
-    <Box className={"MyServiesPage"}>
+    <Box className={"MyServicesPage"}>
       <Navbar isLoggedIn={false}/>
+      <Box className={"MyServicesPage-banner"}>
+        <Typography variant={"h2"} className={"MyServicesPage-title"}>Mis servicios</Typography>
+      </Box>
     </Box>
   )
 }
