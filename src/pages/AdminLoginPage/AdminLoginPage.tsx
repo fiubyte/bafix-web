350650a2a426e72c04db10a6302c6126e558eba4
matias-gonz <maigonzalez@fi.uba.ar>
2024-04-13 20:45:05 +0200

    fiubyte/.github#5 Add login form

--- src/pages/AdminLoginPage/AdminLoginPage.tsx
+++ src/pages/AdminLoginPage/AdminLoginPage.tsx
@@ -1,9 +1,23 @@
-import {Box} from "@mui/material";
+import {Box, Typography} from "@mui/material";
 import "./AdminLoginPage.css"
+import LoginForm from "../../components/LoginForm/LoginForm";
+import {useState} from "react";
 
 const AdminLoginPage = () => {
+
+  const [loginError, setLoginError] = useState(false);
+
   return (
     <Box className={"AdminLoginPage"}>
+      {loginError && (
+        <Box className={"AdminLoginPage-error"}>
+          <Typography variant={"body1"} className={"AdminLoginPage-error-message"}>Error en el inicio de sesión</Typography>
+        </Box>
+      )}
+      <LoginForm handleSubmitError={() => {
+        console.log('Error en el inicio de sesión')
+        setLoginError(true)
+      }}/>
     </Box>
   )
 }
