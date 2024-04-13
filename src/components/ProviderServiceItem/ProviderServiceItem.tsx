b13ca780966053a1cf84f37ebcc78d8c3f959d35
matias-gonz <maigonzalez@fi.uba.ar>
2024-04-13 19:24:46 +0200

    fiubyte/.github#16 Add service item title

--- src/components/ProviderServiceItem/ProviderServiceItem.tsx
+++ src/components/ProviderServiceItem/ProviderServiceItem.tsx
@@ -1,4 +1,5 @@
 import {Box, Typography} from "@mui/material";
+import "./ProviderServiceItem.css";
 
 type Service = {
   title: string;
@@ -14,12 +15,13 @@ type ProviderServiceItemProps = {
   service: Service;
 }
 
-const ProviderServiceItem = ({ service }: ProviderServiceItemProps) => {
+const ProviderServiceItem = ({service}: ProviderServiceItemProps) => {
   return (
     <Box className={"ProviderServiceItem"}>
-      <Box className={"ProviderServiceItem-title"}>
-        <Typography variant={"h4"}>{service.title}</Typography>
-        <Typography variant={"h4"}>{service.service_category.title}</Typography>
+      <Box className={"ProviderServiceItem-title-container"}>
+        <Typography variant={"h3"}>
+          <span className={"ProviderServiceItem-title"}>{service.title}</span>
+          <span className={"ProviderServiceItem-category"}> • {service.service_category.title}</span></Typography>
       </Box>
       <Box className={"ProviderServiceItem-approved"}>
         <Typography variant={"body1"}>{service.approved ? "Aprobado" : "Pendiente de aprobación"}</Typography>
