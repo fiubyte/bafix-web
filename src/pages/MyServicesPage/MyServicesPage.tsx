2c5f795e5b5113bcde957795195d2260d8c87d83
matias-gonz <maigonzalez@fi.uba.ar>
2024-04-13 17:13:37 +0200

    fiubyte/.github#3 Add Agregar nuevo servicio button

--- src/pages/MyServicesPage/MyServicesPage.tsx
+++ src/pages/MyServicesPage/MyServicesPage.tsx
@@ -1,6 +1,7 @@
-import {Box, Typography} from "@mui/material";
+import {Box, Button, Typography} from "@mui/material";
 import Navbar from "../../components/Navbar/Navbar";
 import React from "react";
+import AddIcon from '@mui/icons-material/Add';
 import './MyServicesPage.css';
 
 const MyServicesPage = () => {
@@ -9,6 +10,11 @@ const MyServicesPage = () => {
       <Navbar isLoggedIn={false}/>
       <Box className={"MyServicesPage-banner"}>
         <Typography variant={"h2"} className={"MyServicesPage-title"}>Mis servicios</Typography>
+      </Box>
+      <Box className={"MyServicesPage-button-container"}>
+        <Button variant="contained" startIcon={<AddIcon />} className={"MyServicesPage-button"}>
+          Agregar un nuevo servicio
+        </Button>
       </Box>
     </Box>
   )
