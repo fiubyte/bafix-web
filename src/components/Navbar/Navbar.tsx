173f40e656cf2c79c60e38aa27c06edf0bd535d3
matias-gonz <maigonzalez@fi.uba.ar>
2024-05-06 22:54:54 +0200

    fiubyte/.github#35 Add BAFix logo to navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
-import {AppBar, Toolbar, IconButton, Avatar} from '@mui/material';
+import {AppBar, Toolbar, IconButton, Avatar, Box} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import "./Navbar.css"
+import "./Navbar.css";
+import BaFixLogo from "../../assets/BaFixLogo";
 
 interface NavbarProps {
   isLoggedIn: boolean;
   userPhoto?: string;
 }
 
-const Navbar: React.FC<NavbarProps> = ({ isLoggedIn, userPhoto }) => {
+const Navbar: React.FC<NavbarProps> = ({isLoggedIn, userPhoto}) => {
   return (
     <AppBar position="static">
       <Toolbar className={"Navbar"}>
         <IconButton edge="start" color="inherit" aria-label="menu">
-          <MenuIcon />
+          <MenuIcon/>
         </IconButton>
+
+        <Box className={"Navbar-logo"}>
+          <BaFixLogo/>
+        </Box>
+
+        <Box/>
 
         {isLoggedIn && userPhoto && (
           <Avatar alt="User Photo" src={userPhoto}/>
