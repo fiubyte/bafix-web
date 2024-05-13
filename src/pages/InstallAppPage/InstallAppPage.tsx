662938660adcebc0466e9347cec365895912928e
matias-gonz <maigonzalez@fi.uba.ar>
2024-05-13 11:53:35 +0200

    fiubyte/.github#46 Add Install App page

--- src/pages/InstallAppPage/InstallAppPage.tsx
+++ src/pages/InstallAppPage/InstallAppPage.tsx
@@ -1,9 +1,24 @@
+import {Box} from "@mui/material";
+import {BaFixLettersWhite, BaFixLogo} from "../../assets/Assets";
+import "./InstallAppPage.css";
 
 const InstallAppPage = () => {
   return (
-    <div>
-      <h1>Install App Page</h1>
-    </div>
+    <Box className={"InstallAppPage"}>
+      <Box className={"InstallAppPage-logo-container"}>
+        <Box className={"InstallAppPage-logo"}>
+          <BaFixLogo/>
+        </Box>
+        <Box className={"InstallAppPage-letters"}>
+          <BaFixLettersWhite/>
+        </Box>
+      </Box>
+      <img src={"https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"}
+           alt={"Get it on Play Store"} className={"InstallAppPage-playstore"} onClick={() => {
+        window.open("https://play.google.com/store/apps", "_blank")
+      }}/>
+
+    </Box>
   );
 }
 
