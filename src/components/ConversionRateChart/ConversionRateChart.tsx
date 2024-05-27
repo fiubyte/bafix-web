badb152b4cfc6f5cae86eb0c6425ac28cbfa4653
matias-gonz <maigonzalez@fi.uba.ar>
2024-05-27 09:08:39 +0200

    fiubyte/.github#51 Add groupby

--- src/components/ConversionRateChart/ConversionRateChart.tsx
+++ src/components/ConversionRateChart/ConversionRateChart.tsx
@@ -1,14 +1,35 @@
 "use client";
 
 import {ServiceView} from "../../models/ServiceView";
+import {useEffect, useState} from "react";
 
 interface ConversionRateChartProps {
   views: ServiceView[];
   groupBy: "day" | "month";
 }
 
+interface ConversionRateData {
+  date: string;
+  views: number;
+}
+
 const ConversionRateChart = ({views, groupBy}: ConversionRateChartProps) => {
 
+  const [data, setData] = useState<ConversionRateData[]>([]);
+
+  useEffect(() => {
+    const groupedData: ConversionRateData[] = [];
+    views.forEach(view => {
+      const date = groupBy === "day" ? view.timestamp.toISOString().slice(0, 10) : view.timestamp.toISOString().slice(0, 7);
+      const existingData = groupedData.find(item => item.date === date);
+      if (existingData) {
+        existingData.views++;
+      } else {
+        groupedData.push({date, views: 1,});
+      }
+    });
+    setData(groupedData);
+  }, [views, groupBy]);
 }
 
 export default ConversionRateChart;
