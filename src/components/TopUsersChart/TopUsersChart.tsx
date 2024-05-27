146e7ef73a57146c81dde2a411b1b955b773d34e
matias-gonz <maigonzalez@fi.uba.ar>
2024-05-27 11:07:37 +0200

    fiubyte/.github#50 Add user list

--- src/components/TopUsersChart/TopUsersChart.tsx
+++ src/components/TopUsersChart/TopUsersChart.tsx
@@ -2,8 +2,12 @@
 
 import {useEffect, useState} from "react";
 import {Card, DonutChart} from "@tremor/react";
-import {Grid} from "@mui/material";
+import {Grid, Typography} from "@mui/material";
 import {ServiceContact} from "../../models/ServiceContact";
+import {User} from "../../models/User";
+import config from "../../config";
+import axios from "axios";
+import "./TopUsersChart.css";
 
 interface TopUsersChartProps {
   contacts: ServiceContact[];
@@ -11,11 +15,13 @@ interface TopUsersChartProps {
 
 interface TopUsersData {
   name: string;
+  photo_url: string;
   value: number;
 }
 
 const TopUsersChart = ({contacts}: TopUsersChartProps) => {
   const [data, setData] = useState<TopUsersData[]>([]);
+  const [users, setUsers] = useState<User[]>([]);
 
   useEffect(() => {
     const groupedData: TopUsersData[] = [];
@@ -24,11 +30,24 @@ const TopUsersChart = ({contacts}: TopUsersChartProps) => {
       if (existingData) {
         existingData.value++;
       } else {
-        groupedData.push({name: contact.user_id.toString(), value: 1});
+        groupedData.push({name: contact.user_id.toString(), value: 1, photo_url: ""});
       }
     });
     const sortedData = groupedData.sort((a, b) => b.value - a.value).slice(0, 5);
-    setData(sortedData);
+    const userIds = sortedData.map(item => item.name).join(",");
+    axios.get(`${config.apiUrl}/users?user_ids=${userIds}`,
+      {headers: {"Authorization": `Bearer ${localStorage.getItem(config.LOCAL_STORAGE_JWT_KEY)}`}})
+      .then((response) => {
+          setUsers(response.data);
+          const finalData = sortedData.map(item => {
+            const user = users.find(user => user.id.toString() === item.name);
+            return {name: user?.name || item.name, value: item.value, photo_url: user?.profile_photo_url || ""};
+          });
+          setData(finalData);
+        }
+      ).catch((error) => {
+      console.error(error);
+    });
   }, [contacts]);
 
   return (
@@ -41,11 +60,20 @@ const TopUsersChart = ({contacts}: TopUsersChartProps) => {
           <DonutChart
             variant="pie"
             data={data}
-              className="mt-2 h-80 w-80"
+            className="mt-2 h-80 w-80"
           />
         </Grid>
+        <Grid item xs={6}>
+          <Grid container spacing={2}>
+            {data.map((item, index) => (
+              <Grid item xs={12} key={index}>
+                <img src={item.photo_url} alt={item.name} className={"TopUsersChart-photo"}/>
+                <Typography variant={"h4"}>{item.name} - {item.value} contactos</Typography>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
       </Grid>
-
     </Card>
   );
 
