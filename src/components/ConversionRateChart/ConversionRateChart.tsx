e18af7542bf6deac8b2f4c2803dc3ccb302ff75e
matias-gonz <maigonzalez@fi.uba.ar>
2024-05-27 09:12:08 +0200

    fiubyte/.github#51 Add ServiceContact

--- src/components/ConversionRateChart/ConversionRateChart.tsx
+++ src/components/ConversionRateChart/ConversionRateChart.tsx
@@ -2,18 +2,21 @@
 
 import {ServiceView} from "../../models/ServiceView";
 import {useEffect, useState} from "react";
+import {ServiceContact} from "../../models/ServiceContact";
 
 interface ConversionRateChartProps {
   views: ServiceView[];
+  contacts: ServiceContact[];
   groupBy: "day" | "month";
 }
 
 interface ConversionRateData {
   date: string;
   views: number;
+  contacts: number;
 }
 
-const ConversionRateChart = ({views, groupBy}: ConversionRateChartProps) => {
+const ConversionRateChart = ({views, contacts, groupBy}: ConversionRateChartProps) => {
 
   const [data, setData] = useState<ConversionRateData[]>([]);
 
@@ -25,7 +28,16 @@ const ConversionRateChart = ({views, groupBy}: ConversionRateChartProps) => {
       if (existingData) {
         existingData.views++;
       } else {
-        groupedData.push({date, views: 1,});
+        groupedData.push({date, views: 1, contacts: 0});
+      }
+    });
+    contacts.forEach(contact => {
+      const date = groupBy === "day" ? contact.timestamp.toISOString().slice(0, 10) : contact.timestamp.toISOString().slice(0, 7);
+      const existingData = groupedData.find(item => item.date === date);
+      if (existingData) {
+        existingData.contacts++;
+      } else {
+        groupedData.push({date, views: 0, contacts: 1});
       }
     });
     setData(groupedData);
---
+++ src/models/ServiceContact.tsx
@@ -0,0 +1,6 @@
+export interface ServiceContact {
+  id: number;
+  service_id: number;
+  user_id: number;
+  timestamp: Date;
+}
